18b47f89c61b87e298ae89da8e13cc26c656bfa1
Abdul Hannan Mustajab <35130162+hannanmustajab@users.noreply.github.com>
2021-08-18 01:10:22 +0530

    Updated code and added some comments.

--- passive_infrared_sensor_FOR_MOTION.ino
+++ passive_infrared_sensor_FOR_MOTION.ino
@@ -1,28 +1,30 @@
-int LED =2;
-  uint8_t led = D0;
+  int LED =2;
+  // Declare sensorOutputPin to D0. 
+  uint8_t sensorOutputPin = D0;
   
   
 void setup() {
-  pinMode(D0,INPUT);
+  
+  // Setup sensorOutputPin to INPUT mode. This means it will read value from the sensor. 
+  pinMode(sensorOutputPin,INPUT);
+  
+  // GPIO pin on which LED is connected.
   pinMode(LED,OUTPUT);
+  
+  // Begin Serial Communication.
   Serial.begin(9600);
   
 }
 
 void loop() {
- int sensorvalue = digitalRead(D0);
+ int sensorvalue = digitalRead(sensorOutputPin);
  Serial.println(sensorvalue);
  
- if (sensorvalue== HIGH){
+ if (sensorvalue == HIGH){
   digitalWrite(LED,HIGH);
- delay(1000);
+  delay(1000);
  }
- 
- else
- {
-  digitalWrite(LED,LOW);
- }
-  
-  
+ else if (sensorvalue == LOW) digitalWrite(LED,LOW);
+   
 
 }
